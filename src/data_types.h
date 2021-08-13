15fb4fcaf941bf6a882923e2996f3c41c2456243
salmoncatt <salmoncattyt@gmail.com>
2021-08-12 21:02:15 -0500

    working on making an opengl wgl window

---
+++ src/data_types.h
@@ -0,0 +1,43 @@
+
+#ifndef DATA_TYPES_H
+#define DATA_TYPES_H
+
+#include <stdint.h>
+
+
+//yoinked from tsfoundation.h (heh)
+typedef int8_t    int8;
+typedef int16_t   int16;
+typedef int32_t   int32;
+typedef int64_t   int64;
+typedef uint8_t   uint8;
+typedef uint16_t  uint16;
+typedef uint32_t  uint32;
+typedef uint64_t  uint64;
+typedef float     real32;
+typedef double    real64;
+typedef float     float32;
+typedef double    float64;
+typedef int8      bool8;
+typedef int16     bool16;
+typedef int32     bool32;
+typedef int64     bool64;
+
+typedef int8      i8;
+typedef int16     i16;
+typedef int32     i32;
+typedef int64     i64;
+typedef uint8     u8;
+typedef uint16    u16;
+typedef uint32    u32;
+typedef uint64    u64;
+typedef real32    r32;
+typedef real64    r64;
+typedef float32   f32;
+typedef float64   f64;
+typedef bool8     b8;
+typedef bool16    b16;
+typedef bool32    b32;
+typedef bool64    b64;
+
+#endif //DATA_TYPES_H
--- src/main.c
+++ src/main.c
@@ -3,76 +3,36 @@
 #include <stdlib.h>
 #include <windows.h>
 #include <GL/gl.h>
+#include "data_types.h"
 
 
-#define FG_RED      31
-#define FG_GREEN    32
-#define FG_BLUE     34
-#define FG_DEFAULT  39
-#define BG_RED      41
-#define BG_GREEN    42
-#define BG_BLUE     44
-#define BG_DEFAULT  49
-
-//TODO: f32, b8, etc..
-
+//TODO: FINISH THE THEME
 
 #define assert(expression) if(expression) {} else {  printf("[ERROR ASSERT]: [%s] returned false, in file: [%s], in function: [%s], at line: [%i]\n\n", #expression, __FILE__, __FUNCTION__, __LINE__); exit(-1); }
 
 #define assertf(expression,msg) if(expression) {} else {  printf("[ERROR ASSERT]: [%s] returned false, with message: [%s], in file: [%s], in function: [%s], at line: [%i]\n\n", #expression, msg, __FILE__, __FUNCTION__, __LINE__); exit(-1); }
 
-/*#define static_assertf(expression,msg) if(expression) {} else {  printf("[ERROR ASSERT]: [%s] returned false, with message: [%s], in file: [%s], in function: [%s], at line: [%i]\n\n", #expression, msg, __FILE__, __FUNCTION__, __LINE__); exit(-1); }
-
-#define static_assert(expression) if(expression) {} else {  printf("[ERROR ASSERT]: [%s] returned false, in file: [%s], in function: [%s], at line: [%i]\n\n", #expression, __FILE__, __FUNCTION__, __LINE__); exit(-1); }*/
-
-
+LRESULT CALLBACK wnd_procedure(HWND, UINT, WPARAM, LPARAM){
+    
+    
+}
 
 int main(void) {
     
-    //HDC hdc;
+    //me like main no winmain here bois
+    HINSTANCE instance_handle = GetModuleHandle(NULL);
     
-    //HGLRC hglrc = wglCreateContext(hdc);
-    
-    int a = 1;
-    assertf(a == 2, "goober");
-    
-    //printf("\033[%im goober  \n \n", FG_RED);
-    
-    //initTerminal();
-    //SetConsoleTitle("Mini Desktop App"); 
+    WNDCLASS wndcl;
     
     
-    /*if (!glfwInit())
-        printf("glfw could not be created \n");
-    else
-        printf("glfw was created \n");
     
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    memset(&wndcl, 0, sizeof(wndcl));
     
-    GLFWwindow* window = glfwCreateWindow(400, 600, "goober", NULL, NULL);
+    wndcl.style = CS_OWNDC;
+    wndcl.lpfnWndProc = wnd_procedure;
+    wndcl.hInstance = instance_handle;
+    wndcl.lpszClassName = TEXT("goober");
+    RegisterClass(&wndcl);
     
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
-    {
-        printf("Glad could not be initialized");
-    }    
     
-    if (!window)
-        printf("could not create window \n");
-    else
-        printf("created window \n");
-    
-    glfwMakeContextCurrent(window);
-    glfwSwapInterval(0);
-    glfwShowWindow(window);
-    
-    while (!glfwWindowShouldClose(window)) {
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-    
-    glfwTerminate();*/
-    
-    //system("pause>nul");
 }
