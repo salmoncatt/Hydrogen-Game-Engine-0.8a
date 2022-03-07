3a4d93d30d4190a38076c4b6c8e911104b76a016
salmoncatt <salmoncattyt@gmail.com>
2022-03-06 20:50:37 -0600

    textures, they finally work

--- res/shaders/fragment.glsl
+++ res/shaders/fragment.glsl
@@ -1,16 +1,17 @@
 #version 400 core
+#extension GL_ARB_bindless_texture : require
 
 in vec3 colour;
 in vec2 pass_texture_coords;
 
 out vec4 out_Color;
 
-sampler2D sampler;
+uniform sampler2D sampler;
 
 void main(void){
 	
-	//out_Color = vec4(pass_texture_coords.x, pass_texture_coords.y, 0, 1);
 
 	out_Color = texture(sampler, pass_texture_coords);
+	//out_Color = vec4(pass_texture_coords.x, pass_texture_coords.y, 0, 1);
 
 }
